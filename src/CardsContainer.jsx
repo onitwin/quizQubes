8c5c0ece897c15ed8bcf266504b7ecd13715fc2b
onitwin <onitwin@gmail.com>
2026-08-09 20:16:52 +0100

    update card logic with maps

--- src/CardsContainer.jsx
+++ src/CardsContainer.jsx
@@ -1,28 +1,19 @@
 import { Card } from "./Card";
-import data from "../public/questions.json";
+import data from "./assets/questions.json";
 export const CardsContainer = () => {
   // console.log("Logging Data :", data);
-  const questionSet = data[1];
-  console.log("Question Set:", questionSet.questionSet);
+  const questionSet = data[0].questionSet;
+  console.log("Question Set:", questionSet);
 
-  return (
-    <main className="cardsWrapper">
-      <Card text="What is the capital of France?" />
-      <Card text="Who is the lead singer of 'Foo Fighters?'" />
-      <Card text="Joe Biden" />
-      <Card text="What is the top selling book of all time" />
-      <Card text="The Bible" />
-      <Card textext="Are Whales a fish or a mammal" />
-      <Card text="Dave Grohl" />
-      <Card text="Who won the 2020 US presidential eleection" />
-      <Card text="Las Vegas" />
-      <Card text="What is 4x4" />
-      <Card text="Rings" />
-      <Card text="Paris" />
-      <Card text="Complete the film title: 'Leaving " />
-      <Card text="Mammal" />
-      <Card text="Sonic the Hedgehog collects these" />
-      <Card text="16" />
-    </main>
-  );
+  const questionMap = questionSet.map((q) => {
+    return <Card text={q.question} key={q.pairId} />;
+  });
+
+  const answerMap = questionSet.map((a) => {
+    return <Card text={a.answer} key={parseInt(a.pairId) * 10} />;
+  });
+
+  const combinedCards = [...answerMap, ...questionMap];
+
+  return <main className="cardsWrapper">{combinedCards}</main>;
 };
