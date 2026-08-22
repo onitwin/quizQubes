3dd717e37a6f25e070c4e9ffbe9621dfe6900b32
onitwin <onitwin@gmail.com>
2026-08-22 21:03:50 +0100

    update quiz

--- src/CardsContainer.jsx
+++ src/CardsContainer.jsx
@@ -42,7 +42,7 @@ export const CardsContainer = () => {
     }
   };
 
-  const [questionSet, setQuestionSet] = useState(data[4].questionSet);
+  const [questionSet, setQuestionSet] = useState(data[5].questionSet);
 
   const questionMap = questionSet.map((q) => {
     return (
