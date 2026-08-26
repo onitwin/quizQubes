21244e1d4aaeefd1fe5531680bbdddda8fcd9f1f
onitwin <onitwin@gmail.com>
2026-08-26 09:09:20 +0100

    update quiz and minor column gap

--- src/CardsContainer.jsx
+++ src/CardsContainer.jsx
@@ -42,7 +42,7 @@ export const CardsContainer = () => {
     }
   };
 
-  const [questionSet, setQuestionSet] = useState(data[6].questionSet);
+  const [questionSet, setQuestionSet] = useState(data[4].questionSet);
 
   const questionMap = questionSet.map((q) => {
     return (
