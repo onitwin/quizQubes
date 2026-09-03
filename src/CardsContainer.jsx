c18af64eb5f025d4658f63fa84ca7e8efbc8a128
onitwin <onitwin@gmail.com>
2026-09-03 10:07:27 +0100

    correct minor styling allignment and add new quiz

--- src/CardsContainer.jsx
+++ src/CardsContainer.jsx
@@ -42,7 +42,7 @@ export const CardsContainer = () => {
     }
   };
 
-  const [questionSet, setQuestionSet] = useState(data[10].questionSet);
+  const [questionSet, setQuestionSet] = useState(data[11].questionSet);
 
   const questionMap = questionSet.map((q) => {
     return (
