eac1c2717124e5ac56667ae4e97cdbe18fcb787d
onitwin <onitwin@gmail.com>
2026-09-17 13:42:07 +0100

    update quiz

--- src/CardsContainer.jsx
+++ src/CardsContainer.jsx
@@ -39,7 +39,7 @@ export const CardsContainer = () => {
     }
   };
 
-  const [questionSet, setQuestionSet] = useState(data[17].questionSet);
+  const [questionSet, setQuestionSet] = useState(data[18].questionSet);
 
   const questionMap = questionSet.map((q) => {
     return (
@@ -69,7 +69,7 @@ export const CardsContainer = () => {
 
   return (
     <>
-      <ReusableModal text={data[17].description} />
+      <ReusableModal text={data[18].description} />
       <main className="cardsWrapper">{combinedCards}</main>
       <h2 className="attemptsDisplay">Attempts:{attempts}</h2>
       <FooterContainer />
