c396630130bed0c45ca3f7a43a428ca3932d7191
onitwin <onitwin@gmail.com>
2026-09-20 12:13:36 +0100

    add new quiz

--- src/CardsContainer.jsx
+++ src/CardsContainer.jsx
@@ -39,7 +39,7 @@ export const CardsContainer = () => {
     }
   };
 
-  const [questionSet, setQuestionSet] = useState(data[19].questionSet);
+  const [questionSet, setQuestionSet] = useState(data[20].questionSet);
 
   const questionMap = questionSet.map((q) => {
     return (
@@ -69,7 +69,7 @@ export const CardsContainer = () => {
 
   return (
     <>
-      <ReusableModal text={data[19].description} />
+      <ReusableModal text={data[20].description} />
       <main className="cardsWrapper">{combinedCards}</main>
       <h2 className="attemptsDisplay">Attempts:{attempts}</h2>
       <FooterContainer />
